a1bd0e31e9a4495e61ef68db7df3afdfbd359dce
thesaleslegends <kees.yt.vdn2@gmail.com>
2026-01-29 16:28:28 +0000

    Fix supabase import paths

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-import { supabase } from "./supabase.js";
+import { supabase } from "../supabase.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const emailInput = document.getElementById("email");
